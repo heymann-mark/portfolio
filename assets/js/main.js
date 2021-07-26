f8c4d1bd5efa683e5c1444311972fa02fa99c5dc
Mark <mark.heymann01@gmail.com>
2021-07-25 21:26:06 -0400

    Auto-generated commit [ci skip]

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//import * as variable from 'variableName';
+ console.log("Javascript works!");
+ angular.module('MyApp', [])
+
+ .controller('MyCtrl', [function() {
+     angular.element(document).ready(function () {
+      AOS.init();
+      var scroll = new SmoothScroll('a[href*="#"]');
+
+     });
+ }]);
+
